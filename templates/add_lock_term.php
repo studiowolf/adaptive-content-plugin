04b226479e7e8131b7ad86adc4381783b857799b
Rein Aris <info@reinaris.nl>
2014-08-26 08:53:55 +0200

    Valid html for add_lock_term

--- templates/add_lock_term.php
+++ templates/add_lock_term.php
@@ -3,6 +3,6 @@
         <input name="term_locked" type="checkbox" id="term_locked" value="1" /> 
         Vastzetten
     </label>
-    <input name="term_locked_reference" id="term_locked_reference" type="text" value="" size="40">
+    <input name="term_locked_reference" id="term_locked_reference" type="text" value="" size="40" />
     <p>Deze tag vastzetten met een referentie.</p>
 </div>
